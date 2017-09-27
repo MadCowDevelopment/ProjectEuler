3cab6dbba180a06669a2860439dd81827c8d77a4
Mihi <devdumbblind@googlemail.com>
2017-09-27 12:08:37 +0800

    solved problem 13

---
+++ Runner/Problems/0013/Problem0013.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Runner.Problems
+{
+    public class Problem0013 : IProblem
+    {
+        private List<string> _numbers = new List<string>();
+        public object Solve()
+        {
+            var digits = new List<int>();
+            int borrow = 0;
+            for (int i = 11; i >= 0; i--)
+            {
+                var sum = _numbers.Sum(p => int.Parse(p[i].ToString())) + borrow;
+                var sumString = sum.ToString();
+                var lastDigit = int.Parse(sumString[sumString.Length - 1].ToString());
+                borrow = (sum - lastDigit) / 10;
+
+                digits.Add(lastDigit);
+            }
+
+            if (borrow > 0)
+            {
+                var borrowString = borrow.ToString();
+
+                for (int i = borrowString.Length - 1; i >= 0; i--)
+                {
+                    var digit = int.Parse(borrowString[i].ToString());
+                    digits.Add(digit);
+                }
+            }
+
+            digits.Reverse();
+
+            var result = decimal.Parse(string.Join("", digits.Take(10)));
+
+            return result;
+        }
+
+        public void Initialize()
+        {
+            var lines = File.ReadAllLines(@"Problems\0013\data.txt");
+            _numbers = lines.ToList();
+        }
+    }
+}
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -9,7 +9,7 @@ namespace Runner
     {
         static void Main()
         {
-            SolveProblem<Problem0012>();
+            SolveProblem<Problem0013>();
         }
 
         private static void SolveProblem<TProblem>() where TProblem : IProblem
