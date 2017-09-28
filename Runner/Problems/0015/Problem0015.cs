72838c200d9e42027220f39218ba8a15f85895c4
Mihi <devdumbblind@googlemail.com>
2017-09-28 14:36:37 +0800

    solved problem 15

---
+++ Runner/Problems/0015/Problem0015.cs
@@ -0,0 +1,45 @@
+﻿using System.Numerics;
+
+namespace Runner.Problems
+{
+    public class Problem0015 : Problem
+    {
+        public const int Size = 20;
+
+        public override object Solve()
+        {
+            var coeffs = new BigInteger[Size, Size];
+
+            for (var i = 0; i < Size; i++)
+            {
+                coeffs[0, i] = 1;
+            }
+
+            for (var i = 1; i < Size; i++)
+            {
+                for (var j = 0; j < Size; j++)
+                {
+                    BigInteger current = 0;
+
+                    for (var k = j; k < Size; k++)
+                    {
+                        current += coeffs[i - 1, k];
+                    }
+
+                    coeffs[i, j] = current;
+                }
+            }
+
+            BigInteger sum = 1;
+            for (var i = 0; i < Size; i++)
+            {
+                for (var j = 0; j < Size; j++)
+                {
+                    sum += coeffs[i, j];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -9,7 +9,7 @@ namespace Runner
     {
         static void Main()
         {
-            SolveProblem<Problem0014>();
+            SolveProblem<Problem0015>();
         }
 
         private static void SolveProblem<TProblem>() where TProblem : IProblem
