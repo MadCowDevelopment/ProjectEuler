f0c2ba067667c25e633a86720894ba98b7ab9b0d
Mihi <devdumbblind@googlemail.com>
2017-10-11 13:00:55 +0800

    solved problem 20

---
+++ Runner/Problems/0020/Problem0020.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Numerics;
+
+namespace Runner.Problems
+{
+    public class Problem0020 : Problem
+    {
+        public override object Solve()
+        {
+            BigInteger factorial = 1;
+
+            for (var i = 2; i <= 100; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial.ToString().Select(p => int.Parse(p.ToString()))
+                .Aggregate<int, BigInteger>(0, (current, digit) => current + digit);
+        }
+    }
+}
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -9,7 +9,7 @@ namespace Runner
     {
         static void Main()
         {
-            SolveProblem<Problem0019>();
+            SolveProblem<Problem0020>();
         }
 
         private static void SolveProblem<TProblem>() where TProblem : IProblem
