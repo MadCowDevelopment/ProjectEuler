40d2719ac001d932f31b035fceffa71ffee5b13c
Mihi <devdumbblind@googlemail.com>
2018-12-04 09:12:26 +0800

    added problems 29 and 30

---
+++ Runner/Problems/0026-0050/0029/Problem0029.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Runner.Problems
+{
+    public class Problem0029 : Problem
+    {
+        public override object Solve()
+        {
+            var numbers = new HashSet<double>();
+            for (int a = 2; a <= 100; a++)
+            {
+                for (int b = 2; b <= 100 ; b++)
+                {
+                    var product = Math.Pow(a, b);
+                    if (!numbers.Contains(product)) numbers.Add(product);
+                }
+            }
+
+            return numbers.Count;
+        }
+    }
+}
